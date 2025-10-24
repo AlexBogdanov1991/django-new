9a1c80a3baabac432fedb954db335d00147ff370
BogdanovALTX <sir.bogdanov2015@yandex.ru>
2025-10-24 11:18:48 +0300

    new

--- djangotutorial/polls/admin.py
+++ djangotutorial/polls/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+
+@admin.register(models.Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ["question_text"]
+
+
+@admin.register(models.Choice)
+class ChoiceAdmin(admin.ModelAdmin):
+    list_display = ["question", "choice_text"]
+
+
+@admin.register(models.Answer)
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ["question", "choice"]
